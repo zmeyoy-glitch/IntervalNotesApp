79aa5bd159cc60ec248efa3a42c7f9145e1b03e2
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-19 00:26:10 +0300

    Initial: Theme color definitions

---
+++ app/src/main/java/com/zmeyoyglitch/intervalnotes/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.zmeyoyglitch.intervalnotes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF6C5B7F)
+val Pink40 = Color(0xFFD0A4DE)
+
+val Teal200 = Color(0xFF185042) // Darker teal for dark mode contrast
